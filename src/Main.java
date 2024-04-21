4ed530005098661bcf39d8bb50e50d596f5f01a1
Kennedy-Torres <krtorres.programador@hotmail.com>
2024-04-20 22:46:26 -0300

    Implementação na classe Main - instânciando itens bases das classes testes para main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import Classes.Banco;
+import Classes.Funcionario;
+import Classes.Loja;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! - branch dev");
+        Banco banco = new Banco();
+
+        Loja loja1 = new Loja("Loja 1");
+        Loja loja2 = new Loja("Loja 2");
+
+        // lojas disponiveis
+        Loja[] lojasDisponiveis = {loja1, loja2};
+
+        Funcionario funcionario1 = new Funcionario("Amanda", loja1, banco);
+        Funcionario funcionario2 = new Funcionario("Yasmin", loja1, banco);
+        Funcionario funcionario3 = new Funcionario("José", loja2, banco);
+        Funcionario funcionario4 = new Funcionario("Marcos", loja2, banco);
+
+        // lista de funcionarios da loja1
+        List<Funcionario> funcionariosLoja1 = new ArrayList<>();
+        funcionariosLoja1.add(funcionario1);
+        funcionariosLoja1.add(funcionario2);
+
+        // lista de funcionarios da loja2
+        List<Funcionario> funcionariosLoja2 = new ArrayList<>();
+        funcionariosLoja2.add(funcionario3);
+        funcionariosLoja2.add(funcionario4);
+
     }
 }
