5bce2069be343896c3703e77181c45e0ab89daf5
Kennedy-Torres <krtorres.programador@hotmail.com>
2024-04-20 22:16:27 -0300

    Criação método receberSalario - Transfere o saldo de contaLoja para sua contaSalario (LOJA PAGA SEUS FUNCIONARIOS)

--- src/Classes/Funcionario.java
+++ src/Classes/Funcionario.java
@@ -8,35 +8,55 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Funcionario extends Thread {
     private String nome;
+    private Loja loja;
     private Conta contaSalario;
     private Conta contaInvestimento;
     private Banco banco;
     private final Lock lock = new ReentrantLock(); // Lock para garantir exclusão mutua durante a transferência
 
 
-    public Funcionario(String nome, Banco banco) {
+    public Funcionario(String nome,Loja loja, Banco banco) {
         this.nome = nome;
+        this.loja = loja;
         this.contaSalario = new Conta(nome + " - Salário",0);
         this.contaInvestimento = new Conta(nome + " - Investimento",0);
         this.banco = banco;
+
     }
 
     @Override
     public void run() {
-
+        receberSalario();
         DeContaSalarioParaContaInvestimento();
     }
 
-    // Método para transferir saldo entre as contas do funcionario... fluxo-> contaSalario -> contaInvestimento
+    private void receberSalario() {
+        double salario = 1400.0;
+        lock.lock();
+            try{
+                synchronized (loja) { // sincronizei objeto loja para evitar surpresas de condições adversas geradas no metodo transferir()
+                    if (loja.getContaLoja().getSaldo() >= salario) {
+                        banco.transferir(loja.getContaLoja(), getContaSalario(), salario);
+                        System.out.println("Salário de " + nome + " pago pela " + loja.getNome() + ". Conta de "+nome+" (antes do investimento): R$" + getContaSalario().getSaldo());
+                    } else {
+                        System.out.println("A " + loja.getNome() + " não possui saldo suficiente para pagar o salário de " + getNome() + ". (Saldo atual da " + loja.getNome() + ": R$" + loja.getContaLoja().getSaldo()+")");
+                    }
+                }
+            }finally {
+                lock.unlock();
+            }
+    }
+
+    // metodo para transferir saldo entre as contas do funcionario... fluxo: contaSalario -> contaInvestimento
     public void DeContaSalarioParaContaInvestimento() {
-        lock.lock(); //  o lock  garante a exclusão mútua durante a transferência
+        lock.lock(); // garante exclusão mútua durante a transferência
         try {
             double valorInvestimento = getContaSalario().getSaldo() * 0.2; // investe 20% do salario que a loja deu p/ Funcionario
             if (valorInvestimento == 0 ){
-                System.out.println("Funcionario -" + nome + " não possui saldo suficiente para investir.");
+                System.out.println("Funcionario - " + nome + " não possui saldo suficiente para investir.");
             }else{
                 banco.transferir(contaSalario, contaInvestimento, valorInvestimento);
-                System.out.println("Funcionário - " + nome + " investiu R$" + valorInvestimento + " em sua conta de investimento");
+                System.out.println("\n(Funcionário) - " + nome + " investiu R$" + valorInvestimento+", Saldo atualizado:\n- Conta de investimento = R$"+getContaInvestimento().getSaldo()+" e Conta do salario(líquido) = R$"+getContaSalario().getSaldo());
             }
         } finally {
             lock.unlock();
